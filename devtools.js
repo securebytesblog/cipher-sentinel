e7ba26ed687f1d01965afe0678620294732c0ccb
balachanthar <86591060+balabalachan@users.noreply.github.com>
2025-04-29 12:56:06 +0530

    name corrected

--- devtools.js
+++ devtools.js
@@ -1,9 +1,9 @@
 // Create a new panel in DevTools
 chrome.devtools.panels.create(
-  "SSL Checker",
+  "CipherSentinel",
   "icons/icon16.png",
   "panel.html",
   function(panel) {
-    console.log('SSL Checker panel loaded.');
+    console.log('CipherSentinel panel loaded.');
   }
 );
